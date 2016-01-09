99bfefcf148eee7c778cd19058afcaa383cc2dc4
thomasjacobjr <thomasjacobjr@gmail.com>
2016-01-09 10:52:41 -0700

    Checkpoint 22 Address-Bloc2-Searching: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -26,6 +26,23 @@ class AddressBook
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
-    
   end
-end
+  #Search AddressBook for a specific name by entry
+  def binary_search(name)
+    lower = 0
+    upper = @entries.length - 1
+    while lower <= upper
+      md = (lower + upper) / 2
+      mid_name = @entries[mid].Name
+
+      if name == mid_name
+        return @entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil
+  end
